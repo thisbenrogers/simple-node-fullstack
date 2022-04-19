2698520d864ee9881dd9a5d63e6d103d5af0e940
BenR <20133902+thisbenrogers@users.noreply.github.com>
2022-04-19 11:16:59 -0500

    Edits knexfile to enforce ssl on heroku

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,10 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     migrations: {
       directory: './data/migrations',
     },
